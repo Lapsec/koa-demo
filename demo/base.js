441535113de5b517fa9cacebedfd28ef195a92ec
lapsec <lapse.c@foxmail.com>
2020-02-05 00:11:41 +0800

    feat: first version

--- demo/base.js
+++ demo/base.js
@@ -1,1 +1,22 @@
-console.log('hi')
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200);
+//   res.end('hello world');
+// })
+
+// server.listen(3000, () => {
+//   console.log('server start at 3000');
+// });
+
+const Koa = require('../lib/application').default;
+const app = new Koa();
+
+app.use(async (req, res) => {
+  res.writeHead(200);
+  res.end('Hello world')
+});
+
+app.listen(3001, ()=> {
+  console.log('server start at 3001');
+})
--- src/application.ts
+++ src/application.ts
@@ -1,5 +1,30 @@
 import { EventEmitter } from 'events';
+import http from 'http';
 
-class Application extends EventEmitter {}
+interface Application {
+  callbackFn: (req: object, res: object) => {};
+}
+
+class Application extends EventEmitter implements Application {
+  constructor() {
+    super();
+    this.callbackFn = null;
+  }
+
+  use(fn: () => {}) {
+    this.callbackFn = fn;
+  }
+
+  callback() {
+    return (req: object, res: object) => {
+      this.callbackFn(req, res);
+    };
+  }
+
+  listen(...args: []) {
+    const server = http.createServer(this.callback());
+    return server.listen(...args);
+  }
+}
 
 export default Application;
