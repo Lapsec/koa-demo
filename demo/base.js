1a2d1617e60f5566df30d156b1549bef977289ff
lapsec <lapse.c@foxmail.com>
2020-02-11 23:46:39 +0800

    stash: still somehthing wrong while composing middlewares

--- demo/base.js
+++ demo/base.js
@@ -1,24 +1,16 @@
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-//   res.writeHead(200);
-//   res.end('hello world');
-// })
-
-// server.listen(3000, () => {
-//   console.log('server start at 3000');
-// });
-
 const Koa = require('../lib/application').default;
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+  console.log('1-start');
+  await next();
+  console.log('1-end');
+})
+
 app.use(async (ctx) => {
-  ctx.status = 200;
-  ctx.body = {
-    code: 1,
-    message: 'ok',
-    url: ctx.url
-  };
+  console.log('2-start');
+  ctx.body = 'hello tc';
+  console.log('2-end');
 });
 
 app.listen(3001, () => {
--- src/application.ts
+++ src/application.ts
@@ -8,6 +8,7 @@ interface Application {
   context: typeof context;
   request: typeof request;
   response: typeof response;
+  middleware: any;
 }
 
 class Application extends EventEmitter implements Application {
@@ -17,24 +18,60 @@ class Application extends EventEmitter implements Application {
     this.context = Object.create(context);
     this.request = Object.create(request);
     this.response = Object.create(response);
+    this.middleware = [];
   }
 
-  use(fn: () => {}) {
-    this.callbackFn = fn;
+  use(fn:any) {
+    this.middleware.push(fn); // 存储中间件
+  }
+
+  compose (middleware: any) {
+    if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!')
+    for (const fn of middleware) {
+      if (typeof fn !== 'function') throw new TypeError('Middleware must be composed of functions!')
+    }
+  
+    /**
+     * @param {Object} context
+     * @return {Promise}
+     * @api public
+     */
+  
+    return function (context: any, next: any) {
+      // last called middleware #
+      let index = -1
+      return dispatch(0)
+      function dispatch (i:number) {
+        if (i <= index) return Promise.reject(new Error('next() called multiple times'))
+        index = i
+        let fn = middleware[i]
+        if (i === middleware.length) fn = next
+        if (!fn) return Promise.resolve()
+        try {
+          return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
+        } catch (err) {
+          return Promise.reject(err)
+        }
+      }
+    }
   }
 
   callback() {
-    const handleRequest = (req: any, res: any) => {
+    // 合成所有中间件
+    const fn = this.compose(this.middleware);
+
+    const handleRequest = (req:object, res: object) => {
       const ctx = this.createContext(req, res);
-      return this.handleRequest(ctx);
-    }
+      return this.handleRequest(ctx, fn)
+    };
+
     return handleRequest;
   }
 
-  handleRequest(ctx: any) {
+  handleRequest(ctx: object, fnMiddleware: any) {
     const handleResponse = () => respond(ctx);
-    // callbackFn是个async函数，最后返回promise对象
-    return this.callbackFn(ctx).then(handleResponse);
+    // 执行中间件并把最后的结果交给respond
+    return fnMiddleware(ctx).then(handleResponse);
   }
 
   createContext(req: any, res: any) {
